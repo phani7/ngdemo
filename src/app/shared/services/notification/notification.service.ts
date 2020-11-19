78ea9a5ca5f802154da779b4e666d0e41cb52a4f
Phani <phani7@outlook.com>
2020-11-19 18:03:38 +0530

    minor change

--- src/app/shared/services/notification/notification.service.ts
+++ src/app/shared/services/notification/notification.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(public swUpdate: SwUpdate) {
+    let a = this.swUpdate.isEnabled;
+  }
 }
